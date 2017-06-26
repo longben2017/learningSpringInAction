d89c6a12e0f0331c21613d278b957ee5a64817eb
longben2017 <479501260@qq.com>
2017-06-27 01:02:36 +0800

    spring web application

---
+++ src/main/java/com/learningSpring/chap5/config/SpittrWebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.learningSpring.chap5.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.learningSpring.chap5.web.WebConfig;
+
+public class SpittrWebAppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[]{RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[]{WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
